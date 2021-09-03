9504b48001f8a4f05240db1ce298f0f3def02984
Somesh <somesh_nj2_076@masai.school>
2021-09-03 23:00:45 +0530

    add email

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ menuBtn.onclick = function () {
 	menuBtn.style.opacity = "0";
 	menuBtn.style.pointerEvents = "none";
 	body.style.overflow = "hidden";
-	scrollBtn.style.pointerEvents = "none";
 };
 cancelBtn.onclick = function () {
 	navBar.classList.remove("active");
 	menuBtn.style.opacity = "1";
 	menuBtn.style.pointerEvents = "auto";
 	body.style.overflow = "auto";
-	scrollBtn.style.pointerEvents = "auto";
 };
 
-// Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
 	navLinks[i].addEventListener("click", function () {
@@ -26,4 +23,32 @@ for (var i = 0; i < navLinks.length; i++) {
 		menuBtn.style.opacity = "1";
 		menuBtn.style.pointerEvents = "auto";
 	});
+}
+function sendEmail() {
+	let name = document.querySelector("#name").value;
+	let email = document.querySelector("#email").value;
+	let message = document.querySelector("#message").value;
+	if (name.length === 0) {
+		alert("Enter Name");
+		return;
+	}
+	if (email.length === 0) {
+		alert("Enter E-mail");
+		return;
+	}
+	if (email.includes("@") === false || email.includes(".") === false) {
+		alert("Invalid email");
+		return;
+	}
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "someshkumar71524@gmail.com",
+		Password: "Mansi@123",
+		To: "portfolioccnt@gmail.com",
+		From: "someshkumar71524@gmail.com",
+		Subject: "From portfolio",
+		Body: `Name - ${name}
+		email -${email}
+		message -${message}`,
+	}).then((message) => alert("mail sent successfully"));
 }
