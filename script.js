40c27abe4cbc5061ed544e997c06b2fbc6a2c046
Somesh <somesh_nj2_076@masai.school>
2021-09-14 23:20:24 +0530

    scroll commit

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
+let scrollBtn = document.querySelector(".scroll-button a");
 menuBtn.onclick = function () {
 	navBar.classList.add("active");
 	menuBtn.style.opacity = "0";
 	menuBtn.style.pointerEvents = "none";
-	body.style.overflow = "hidden";
+	body.style.overflow = "scroll";
+	scrollBtn.style.pointerEvents = "auto";
 };
 cancelBtn.onclick = function () {
 	navBar.classList.remove("active");
 	menuBtn.style.opacity = "1";
 	menuBtn.style.pointerEvents = "auto";
-	body.style.overflow = "auto";
+	body.style.overflow = "scroll";
+	scrollBtn.style.pointerEvents = "auto";
 };
 
 let navLinks = document.querySelectorAll(".menu li a");
